97ec841e75d737c6b95b982ad6346728a1887428
eirikrbe <ericfabian10@gmail.com>
2026-04-19 10:19:03 +1200

    update 2
    Update 2 Using class

--- W2/W2Act2.py
+++ W2/W2Act2.py
@@ -1,28 +1,43 @@
 
+from turtle import st
+
+
+students = []
+
+class Student:
+    def set_data(self, name, age, student_number):
+        self.name = name
+        self.age = age
+        self.student_number = student_number
+
+    def display_data(self):
+        return f"Name: {self.name}, Age: {self.age}"
+
 
 def get_info_students():
-    students = []
-    print("How many students are there?")
+    
+    print("How many students?")
     num_students = int(input())
+
     for i in range(num_students):
         name = input(f"Enter the name of student {i + 1}: ")
-        students.append(name)
         age = input(f"Enter the age of student {i + 1}: ")
-        students.append(age)
         student_number = input(f"Enter the student ID of student {i + 1}: ")
-        students.append(student_number)
-    return students
+
+        student = Student()
+        student.set_data(name, age, student_number)
+        students.append(student)
+
+        print()
 
 def display_students(students):
     print("\nStudent Information:")
-    new_list = sorted(students)
-    for i in range(0, len(new_list), 3):
-        print(f"Name: {new_list[i]}, Age: {new_list[i + 1]}, Student ID: {new_list[i + 2]}")
+    new_list = sorted(students, key=lambda s: s.age)
 
-def main():
-    print("Week 2 – Activity 2: Student Information")
-    students = get_info_students()
-    display_students(students)
+    for student in new_list:
+        print(student.display_data())
+
 
 if __name__ == "__main__":
-    main()
+    get_info_students()
+    display_students(students)
