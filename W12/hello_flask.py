4521462c45ad1a244477dbe2d5938b8afa614958
eirikrbe <ericfabian10@gmail.com>
2026-06-27 10:27:23 +1200

    css

--- W12/hello_flask.py
+++ W12/hello_flask.py
@@ -14,7 +14,29 @@ def say_bye():
 
 @app.route("/username/<name>")
 def learn(name):
-    return f"<p>{name} is learning Flask!</p>"
+    return f"""
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>{name} is learning Flask</title>
+            <style>
+                html {{
+                    background-color: green;
+                }}
+                h1, p, a {{
+                    color: red;
+                }}
+            </style>
+        </head>
+        <body>
+            <h1>{name} is learning Flask</h1>
+            <p>{name} is learning Flask with
+               <a href='https://flask.palletsprojects.com/en/stable/quickstart/#'>Flask Documentation</a>
+            </p>
+        </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
